d2c954e7e2c844146452a7e694e04ad49ccbfeb6
UnrelatedString <unrelatedstring@gmail.com>
2021-06-13 04:42:32 -0400

    enlightenment

--- src/instances/objects/player/basic_inner_rat.gd
+++ src/instances/objects/player/basic_inner_rat.gd
@@ -5,5 +5,5 @@ export var ass = Vector2(0,0)
 
 func _physics_process(delta):
     var force = position.direction_to(center.position) * .1
-    force *= position.distance_to(center.position) - 100
+    force *= position.distance_to(center.position) - center.wheel_radius
     add_force(ass, force)
