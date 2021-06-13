30e70f6346ee7d8b4c95a9bf67757fc9bb694f7b
daatguy <daatguy@outlook.com>
2021-06-13 14:17:31 -0400

    asdsd

---
+++ src/instances/objects/enemies/genericenemy/genericenemy.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export var player_range = 100
+export var post_this_bullet : PackedScene
+
+func _physics_process(delta):
+    var vel = ($"../player".get_global_position()-get_global_position()).normalized()*32
+    move_and_slide(vel)
+    if(randf()>0.995):
+        shoot()
+
+func shoot():
+    var bullet = post_this_bullet.instance()
+    bullet.vel = ($"../player".get_global_position()-get_global_position()).normalized()*2
+    bullet.position = get_global_position()
+    $"..".call_deferred("add_child", bullet)
