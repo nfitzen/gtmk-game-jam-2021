49e0dacdf83ac0db8f7407a2566ce21c4fcd94c0
daatguy <daatguy@outlook.com>
2021-06-13 14:07:05 -0400

    barely works

--- src/instances/objects/player/bullet/playerbullet.gd
+++ src/instances/objects/player/bullet/playerbullet.gd
@@ -7,11 +7,14 @@ extends Area2D
 var vel : Vector2 = Vector2(0,0)
 
 func _ready():
-    connect("area_entered", self, "_on_area_entered")
+    connect("body_entered", self, "_on_area_entered")
 
 func _physics_process(delta):
     position+= vel
 
 func _on_area_entered(body):
-    body.queue_free()
+    if(body.is_in_group("bannerenemy")):
+        body.health -=1
+    else:
+        body.queue_free()
     queue_free()
