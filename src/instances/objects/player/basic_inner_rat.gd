6561f215abb7ce213f98551a33573e29af70fba0
UnrelatedString <unrelatedstring@gmail.com>
2021-06-13 05:28:43 -0400

    a step in the wrong direction but holy fuck is it funny

--- src/instances/objects/player/basic_inner_rat.gd
+++ src/instances/objects/player/basic_inner_rat.gd
@@ -5,13 +5,15 @@ export var ass = Vector2(0,0)
 
 
 func _physics_process(delta):
-    linear_velocity = position.direction_to(center.position) * (
+    var to_center = position.direction_to(center.position)
+    linear_velocity = to_center * (
             position.distance_to(center.position) - center.wheel_radius
         ) * 9
     if position.distance_to(center.position) < center.wheel_radius:
         linear_velocity *= 3 * len(center.rats)
     for rat in center.rats:
         if rat != self:
-            var nudge = rat.position.direction_to(position) * 500000
-            nudge /= position.distance_squared_to(rat.position)
+            var nudge = rat.position.direction_to(position) * 50000
+            nudge *= nudge.normalized().dot(to_center)
+            nudge /= position.distance_to(rat.position)
             linear_velocity += nudge
