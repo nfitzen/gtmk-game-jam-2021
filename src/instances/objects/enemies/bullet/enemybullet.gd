a0bb3eeb6d3da82916199b8f39b08308d743a6c7
daatguy <daatguy@outlook.com>
2021-06-13 13:59:01 -0400

    sdsd

--- src/instances/objects/enemies/bullet/enemybullet.gd
+++ src/instances/objects/enemies/bullet/enemybullet.gd
@@ -4,10 +4,14 @@
 
 extends Area2D
 
+var vel : Vector2 = Vector2(0,0)
+
 func _ready():
     connect("area_entered", self, "_on_area_entered")
 
+func _physics_process(delta):
+    position+= vel
+    
 func _on_area_entered(body):
-    print("hi")
     $"../player".remove_rat()
     queue_free()
