dff48c740dab8dc8504be6d9710a8adaa82e402a
UnrelatedString <unrelatedstring@gmail.com>
2021-06-13 01:55:22 -0400

    more trolling

--- src/instances/objects/player/player.gd
+++ src/instances/objects/player/player.gd
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-extends Node2D
+extends KinematicBody2D
 
 export var speed = 220
 export var vertical_mul = 1.0 # if we want to account for the perspective or some shit
@@ -10,11 +10,12 @@ export var vertical_mul = 1.0 # if we want to account for the perspective or som
 func _ready():
     pass # Replace with function body.
 
-func _process(delta):
+func _physics_process(delta):
     var right = int(Input.is_action_pressed("right"))
     var left = int(Input.is_action_pressed("left"))
     var up = int(Input.is_action_pressed("up"))
     var down = int(Input.is_action_pressed("down"))
-    var velocity = Vector2(right-left, down-up).normalized() * delta * speed
+    var velocity = Vector2(right-left, down-up).normalized() * speed
     velocity.y *= vertical_mul
-    position += velocity
+    move_and_slide(velocity)
+    print(position)
